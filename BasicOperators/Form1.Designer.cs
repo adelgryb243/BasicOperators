6c02279527cfd6fa7d11918528437d6a0a796180
Adele Gryba <adelgryb243@ed.amdsb.ca>
2020-02-11 12:31:56 -0500

    Add project files.

---
+++ BasicOperators/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace BasicOperators
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.outputLabel.Location = new System.Drawing.Point(357, 53);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(344, 302);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "outputLabel";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(66, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "wayne";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(66, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "circle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(66, 249);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 61);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "carpet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(66, 327);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(193, 68);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "bill";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "mathOperations";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ BasicOperators/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicOperators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //wayne Gretzky
+            string wayne = "Wayne Gretzky";
+            int jersy = 99;
+
+
+            outputLabel.Text = wayne + " is number " + jersy;
+           
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //area of a circle
+            double pi;
+            pi = 3.14;
+
+            double radius;
+            radius = 15.00;
+
+            double area;
+            area = pi * (radius * radius);
+
+            outputLabel.Text = "The area of a circle with a radius of " + radius + " is " + area;
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //carpet
+
+            double length;
+            length = 8.50;
+
+            double width;
+            width = 6.00;
+
+            double area;
+            area = width * length;
+
+            double costPerMetre;
+            costPerMetre = 19.95;
+
+            double totalCost;
+            totalCost = area * costPerMetre;
+
+            outputLabel.Text = "\nThe area of a room with the dimensions " + length + " by " + width + " is " + area;
+            outputLabel.Text += "\n\n The cost to carpet this room at " + costPerMetre + " is " + totalCost;
+
+
+
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            //bill calculator
+
+            double shirt;
+            shirt = 12.49;
+
+            double cash;
+            cash = 20.00;
+
+            double tax;
+            tax = 0.13;
+
+            double subTax;
+            subTax = shirt * tax;
+
+            double subTotal;
+            subTotal = subTax + shirt;
+
+            double change;
+            change = cash - subTotal;
+
+            outputLabel.Text = "Bill of Charge";
+            outputLabel.Text += "\n\n Shirt:         " + shirt;
+            outputLabel.Text += "\n\n\n Tax:          " + subTax;
+            outputLabel.Text += "\n\n\n\nTotal:        " + subTotal;
+            outputLabel.Text += "\n\n\n\n\n\n Tendered:        " + cash;
+            outputLabel.Text += "\n\n\n\n\n\n\n Change:           " + change;
+
+        }
+    }
+}
